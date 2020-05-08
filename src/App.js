326cbc3e576298795f11a7dc2cd4553d6b9cf090
maribelcuales <maribelcuales@gmail.com>
2020-05-08 16:34:28 +0800

    Import useState hook and establish application's state for home score and away score.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeScore, setHomeScore] = useState(32);
+  const [awayScore, setAwayScore] = useState(32);
 
   return (
     <div className="container">
