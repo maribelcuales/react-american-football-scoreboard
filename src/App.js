8dcb11b4ed505556e5abc2125548dbc8b91822c6
maribelcuales <maribelcuales@gmail.com>
2020-05-08 17:24:29 +0800

    Attached setHomeScore to onClick listener on touchdown button.

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 6)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
